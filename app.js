4e7b2c814e87e12912454233231428fe2e91040e
Bluum <bluumvr@gmail.com>
2023-08-31 14:19:21 +0100

    Edited the app.js

--- app.js
+++ app.js
@@ -1,51 +1,62 @@
 const express = require('express');
+const request = require('request')
 const bodyParser = require('body-parser');
 const path = require('path');
+const { response } = require('express');
 // const fetch = require('node-fetch');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Signup Route
-app.post('/signup', (req, res) => {
+app.post('/subscribe', (req, res) => {
   const { email } = req.body;
 
-  // Make sure fields are filled
-  if (!email) {
-    res.redirect('/fail.html');
-    return;
-  }
-
-  // Construct req data
-  const data = {
+  const mcData = {
     members: [
       {
         email_address: email,
-        status: 'subscribed'
-      }
+        status: 'pending'
+      } 
     ]
-  };
+  }
 
-  const postData = JSON.stringify(data);
+  const mcDataPost = JSON.stringify(mcData);
 
-  fetch('https://us21.api.mailchimp.com/3.0/lists/6270c3c791.', {
-    method: 'POST',
+  const options = {
+    url: 'https://us21.api.mailchimp.com/3.0/lists/6270c3c791',
+    method: 'POST', 
     headers: {
-      Authorization: 'auth 9be5e4a7137a5d54c9c18a8b03c9dec7-us21'
+      Authorization: 'auth be0005fcf036d1f994872a464307d393-us21'
     },
-    body: postData
-  })
-    .then(res.statusCode === 200 ?
-      res.redirect('/success.html') :
-      res.redirect('/fail.html'))
-    .catch(err => console.log(err))
+    body: mcDataPost
+  }
+
+  if (email) {
+    request(options, (err, response, body) => {
+      if(err) {
+        res.json({error: err})
+      }else {
+        if (js) {
+          res.sendStatus(200);
+        }else {
+          res.redirect('/success.html')
+        }
+      }
+    })
+  }else{
+    res.redirect('/fail.html');
+  }
+  
 })
 
+  
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
